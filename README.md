d0bb0536aa43dc806b6ced89dbed92436e50238e
smudge.io <support@smudge.io>
2020-05-19 11:34:34 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # aws-route53-migration
+
+Curently only supports public hosted zones, no provision has been made for private hosted zones.
+
+AWS Reference: https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/hosted-zones-migrating.html
+
+##Export a list of hosted zones
+From the AWS CLI list the HostedZone Id and Name for the existing (source) hosted zones:
+
+  aws route53 list-hosted-zones --output text --query "HostedZones[].[Id,Name]"
+
+## Export each hosted zone
+From the AWS CLI for each hosted zone:
+
+  aws route53 list-resource-record-sets --hosted-zone-id <hosted-zone-id> > <path-to-output-file>
+
+## Migrate exported file for import (Route53Migration.ps1)
+With all of the output files from above in a folder, run the following from an Administrative PowerShell command prompt:
+
+  .\Route53Migration.ps1 -InFolder <source folder>  -OutFolder <destination folder>
+
+## Create hosted zone in new account
+From the AWS CLI for each hosted zone:
+
+  aws route53 create-hosted-zone --name <domain name> --caller-reference "%time%"
+
+##Export a list of hosted zones
+From the AWS CLI list the HostedZone Id and Name of the newly created (destination) hosted zones:
+
+  aws route53 list-hosted-zones --output text --query "HostedZones[].[Id,Name]"
+
+
+## Import converted ResourceRecordSets
+Using the converted text files from the destination directory to create the ResourceRecordSets.
+From the AWS CLI for each hosted zone:
+
+  aws route53 change-resource-record-sets --hosted-zone-id <new zone id> --change-batch file://domain.com.txt
---
+++ Route53Migration.ps1
@@ -0,0 +1,90 @@
+﻿
+<#
+
+.SYNOPSIS
+Updates an exported Route 53 resourcce record set file to be reimported to another Route 53 hosted zone.
+
+.DESCRIPTION
+Creates a new file of resource record sets ready to import from the file exported by the AWS CLI:
+
+    aws route53 list-resource-record-sets --hosted-zone-id hosted-zone-id > path-to-output-file
+
+Sets the Changes to be applied, and the ResoureRecordSets to be created, while excluding the NS for the root of the zone 
+and the SOA resource records that are created when the new hosted zone is created. 
+
+Private hosted zones are not currently supported.
+
+The full procedure is detailed at: https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/hosted-zones-migrating.html
+
+.PARAMETER InFolder 
+The folder of hosted zone resource record sets previously exported via the AWS CLI
+
+.PARAMETER OutFolder
+The destination folder to create the resource record set converted files to be imported into the new hosted zones in the destination account
+
+.EXAMPLE
+Migrate all exported resource record sets exported previously to the .\source folder to new files in the .\destintion folder:
+
+    Route53Migration.ps1 -InFolder .\source -OutFolder .\destination
+
+.OUTPUTS
+Each resource record included in the -OutFolder file is displayed, and the output file ready to create ResourceRecordSets.
+
+.NOTES
+Hosted zones in the destination account need to be created in advance.
+
+#>
+
+Param ([Parameter(Mandatory=$true)][string]$InFolder, 
+    [Parameter(Mandatory=$true)][string]$OutFolder
+)
+
+Function MigrateResorceRecordSets{
+([Parameter(Mandatory=$true)][string]$InFile, 
+    [Parameter(Mandatory=$true)][string]$OutFile
+)
+    # Import JSON as PSObject
+    $inputZoneZone = Get-Content $InFile | ConvertFrom-Json
+
+    # Create an empty PSObject
+    $outputZone = New-Object PSObject 
+    $outputZone | Add-Member -type NoteProperty -name Changes -Value @()
+
+    $domain = ""
+
+    # Loop through ResourceRecordSets to fine SOA
+    $inputZoneZone.ResourceRecordSets | ForEach-Object {
+        # If  SOA record get the domain
+        if ($_.Type -eq "SOA") {
+            $domain = $_.Name
+        }
+    }
+
+    if ($domain -ne "") {
+        # Loop through ResourceRecordSets
+        $inputZoneZone.ResourceRecordSets | ForEach-Object {
+            # If not a NS of the root zone or the SOA record add it to the new object
+            if ((($_.Type -eq "NS") -and ($_.Name -ne $domain)) -or (($_.Type -ne "SOA") -and ($_.Type -ne "NS"))) {
+                Write-Host "Creating:" $domain $_.Name"($($_.Type))"
+                # Create new PSObject
+                $resourceRecord = New-Object PSObject 
+                # Set the Action to CREATE
+                $resourceRecord | Add-Member -type NoteProperty -name Action -Value "CREATE"
+                # Set the ResourceRecordSet to the imported 
+                $resourceRecord | Add-Member -type NoteProperty -name ResourceRecordSet -Value $_ 
+                $outputZone.Changes += $resourceRecord
+            } else {
+                Write-Host "Excluding:" $domain $_.Name "($($_.Type))"
+            }
+        }
+    }
+
+    $outputZone | ConvertTo-Json -depth 10 | Set-Content -Path $OutFile
+}
+
+Get-ChildItem $InFolder | Foreach-Object {
+    $inFile = $_.FullName
+    $outFile = $OutFolder + "\" + $_.Name
+
+    $result = MigrateResorceRecordSets -Infile $inFile -OutFile $outFile
+}
